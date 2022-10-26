3ebeb97a1e29484889c33227cc1bbc27eb9b6c2e
Marc Pérez de Tudela <mperezdetudelaaznar@gmail.com>
2022-10-26 22:00:06 +0200

    style: add global style

--- src/providers/themeProvider.tsx
+++ src/providers/themeProvider.tsx
@@ -1,12 +1,16 @@
 import { ThemeProvider } from 'styled-components'
 import { theme } from '@/styles/theme'
+import { GlobalStyle } from '@/styles/globalStyle'
 
 interface Props {
   children: React.ReactNode
 }
 
 const Theme = ({ children }: Props) => (
-  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    {children}
+  </ThemeProvider>
 )
 
 export { Theme }

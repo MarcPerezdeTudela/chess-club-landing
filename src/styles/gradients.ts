7dd4e063bf0655de0783f1599eeeebf1cc063b5c
Marc Pérez de Tudela <mperezdetudelaaznar@gmail.com>
2022-10-27 11:19:25 +0200

    style: add gradients to theme

---
+++ src/styles/gradients.ts
@@ -0,0 +1,3 @@
+export const gradients = {
+  bodyBackground: 'linear-gradient(180deg, #080D18 0%, #111C33 100%)',
+}
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,12 +2,14 @@ import { colors } from './colors'
 import { spaces } from './spaces'
 import { fonts } from './fonts'
 import { fontSizes } from './fontSizes'
+import { gradients } from './gradients'
 
 const theme = {
   colors,
   spaces,
   fonts,
   fontSizes,
+  gradients,
 }
 
 export { theme }

d05f3bc0a5ad884e60535314a297d965a7052633
Marc Pérez de Tudela <mperezdetudelaaznar@gmail.com>
2022-11-09 22:33:46 +0100

    feat: add functionality to SubscibeForm

--- src/components/common/SubscribeForm.tsx
+++ src/components/common/SubscribeForm.tsx
@@ -1,39 +1,96 @@
-import React from 'react'
-import styled from 'styled-components'
-
-const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault()
-}
+import { Formik, Form, Field } from 'formik'
+import styled, { useTheme } from 'styled-components'
+import confetti from 'canvas-confetti'
+import toast from 'react-hot-toast'
+import * as Yup from 'yup'
+import { ThemeInterface } from '@/types/theme'
 
 export const SubscribeForm = () => {
+  const theme = useTheme() as ThemeInterface
+  const formSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Required'),
+  })
   return (
-    <Form onSubmit={handleOnSubmit}>
-      <EmailInput placeholder="Your email" />
-      <Button>Subscribe</Button>
-    </Form>
+    <Formik
+      initialValues={{
+        email: '',
+      }}
+      validationSchema={formSchema}
+      validateOnChange={false}
+      validateOnBlur={false}
+      onSubmit={(values, { resetForm }) => {
+        void confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+        })
+        toast.success('Successfully subscribed', {
+          position: 'bottom-center',
+          style: {
+            backgroundColor: theme.colors.primaryLight,
+            color: theme.colors.white,
+            fontFamily: theme.fonts.primary,
+          },
+          iconTheme: {
+            primary: theme.colors.secondary,
+            secondary: theme.colors.primaryDark,
+          },
+          duration: 3000,
+        })
+
+        resetForm()
+      }}
+    >
+      {({ errors }) => (
+        <EmailForm>
+          <EmailBar>
+            <EmailInput name="email" placeholder="Your email" />
+            <Button type="submit">Subscribe</Button>
+          </EmailBar>
+          <ErrorMessage>{errors.email}</ErrorMessage>
+        </EmailForm>
+      )}
+    </Formik>
   )
 }
 
-const Form = styled.form`
+const EmailForm = styled(Form)`
+  width: 100%;
+  max-width: 420px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: ${({ theme }) => theme.spaces.xs};
+`
+const EmailBar = styled.div`
   width: 100%;
   max-width: 420px;
   display: flex;
   justify-content: center;
 `
 
-const EmailInput = styled.input`
+const ErrorMessage = styled.span`
+  width: 100%;
+  max-width: 420px;
+  color: tomato;
+  height: ${({ theme }) => theme.fontSizes.text};
+  padding: 0 ${({ theme }) => theme.spaces.xs};
+  font-family: ${({ theme }) => theme.fonts.primary};
+  font-size: ${({ theme }) => theme.fontSizes.text};
+`
+const EmailInput = styled(Field)`
   background-color: ${({ theme }) => theme.colors.white};
   padding: 0 ${({ theme }) => theme.spaces.xs};
   flex-grow: 1;
   height: 42px;
-  border: none;
   font-family: ${({ theme }) => theme.fonts.primary};
   font-size: ${({ theme }) => theme.fontSizes.text};
   color: ${({ theme }) => theme.colors.primaryDark};
+  border: 1px solid ${({ theme }) => theme.colors.secondary};
   border-radius: 5px 0 0 5px;
+
   &:focus {
     outline: none !important;
-    border: 1px solid ${({ theme }) => theme.colors.secondary};
     box-shadow: 0 0 4px ${({ theme }) => theme.colors.secondary};
   }
 `
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { Theme } from '@/providers/themeProvider'
 import { Layout } from '@/layouts/MainLayout/Layout'
+import { Toaster } from 'react-hot-toast'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
@@ -10,6 +11,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <title>Chess Club</title>
       </Head>
       <Theme>
+        <Toaster />
         <Layout>
           <Component {...pageProps} />
         </Layout>
